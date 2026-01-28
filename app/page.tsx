7b9c4139c1afc71c3e5b887ba06510753fd4441b
NIHAL <nilufdev@gmail.com>
2026-01-29 00:35:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Algoslayer Archive
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Welcome to the official problem repository for the Algoslayer coding events at Thejus Engineering College. Select an event below to access the problem statements and editorial breakdowns for that year.`}
       </p>
       <div className="my-8">
         <BlogPosts />
